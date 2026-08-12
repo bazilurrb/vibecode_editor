4d23bab15f869a68a92bf1381bc3092f563682b6
Bazilur Rahman Bazigh <bazilur.rb@gmail.com>
2026-08-13 03:28:28 +0530

    dashboard 2

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,33 @@
 import { SidebarProvider } from '@/components/ui/sidebar'
 import DashboardSidebar from '@/features/dashboard/components/dashboard-sidebar'
 import React from 'react'
+import { getAllPlaygroundForUser } from '@/features/dashboard/actions'
 
 export default async function DashboardLayout({children}: {children:React.ReactNode}){
+
+    const playgroundData = await getAllPlaygroundForUser();
+
+    const technologyIconMap: Record<string, string>={
+        REACT:"Zap",
+        NEXTJS:"Lightbulb",
+        EXPRESS:"Database",
+        VUE:"Compass",
+        HONO:"FlameIcon",
+        ANGULAR:"Terminal",
+    }
+
+    const formattedPlaygroundData = playgroundData?.map((playground)=>({
+        id:playground.id,
+        name:playground.title,
+        starred:playground.Starmark?.[0]?.isMarked || false,
+        icon: technologyIconMap[playground.template] || "Code2",
+    })) || [];
+
     return (
         <SidebarProvider>
             <div className='flex min-h-screen w-full overflow-x-hidden'>
                 {/* TODO: DASHBOARDSIDEBAR implement */}
-                <DashboardSidebar initialPlaygroundData={[]}/>
+                <DashboardSidebar initialPlaygroundData={formattedPlaygroundData}/>
                 <main className='flex-1'>
                     {children}
                 </main>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,11 @@ import EmptyState from '@/components/ui/empty-state'
 import React from 'react'
 import AddNewButton from '@/features/dashboard/components/add-new-button'
 import AddRepoButton from '@/features/dashboard/components/add-repo-button'
+import { deleteProjectById, duplicateProjectById, editProjectById, getAllPlaygroundForUser } from '@/features/dashboard/actions'
+import ProjectTable from '@/features/dashboard/components/project-table'
 
-const Page = () => {
-
-    const playgrounds:any[] = [];
+const Page = async () => {
+    const playgrounds = await getAllPlaygroundForUser();
   return (
     <div className='flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
@@ -15,11 +16,16 @@ const Page = () => {
 
         <div className='mt-10 flex flex-col justify-center items-center w-full'>
             {
-                playgrounds && playgrounds.length ===0? (<EmptyState title='No Projects fFound' description='Create a new project to get started' imageSrc='/empty-state.svg'/>) :(
+                playgrounds && playgrounds.length ===0? (<EmptyState title='No Projects Found' description='Create a new project to get started' imageSrc='/empty-state.svg'/>) :(
                     //todo add playgrounnd table
-                    <p>
-                        Playground Table
-                    </p>
+                    <ProjectTable
+                    // @ts-ignore
+                    // NB: Need to Update the Types of the playground
+                    projects={playgrounds || []}
+                    onDeleteProject={deleteProjectById}
+                    onUpdateProject={editProjectById}
+                    onDuplicateProject={duplicateProjectById}
+                    />
                 )
             }
         </div>
