4f8c3c9dedb5df56903dec5924f8a8ca3aef135a
Bazilur Rahman Bazigh <bazilur.rb@gmail.com>
2026-08-18 16:05:22 +0530

    Template Selection

--- features/dashboard/actions/index.ts
+++ features/dashboard/actions/index.ts
@@ -9,8 +9,7 @@ export const createPlayground = async(data:{
     title: string;
     template: Templates;
     description?: string;
-    userId: string;
-})=>{
+})=>{ 
     const {template, title, description} = data;
 
     const user = await currentUser();
