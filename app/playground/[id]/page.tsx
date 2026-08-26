47b3b0a7bfdae67e2995eb56fbf83600073e8158
Bazilur Rahman Bazigh <bazilur.rb@gmail.com>
2026-08-26 18:10:28 +0530

    playground editor file explorer 2

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,20 +2,44 @@
 import { SidebarTrigger, SidebarInset } from "@/components/ui/sidebar";
 import { useParams } from "next/navigation"
 import React from "react"
+import { useFileExplorer } from "@/features/playground/hooks/useFileExplorer";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Separator } from "@/components/ui/separator";
 import { usePlayground } from "@/features/playground/hooks/usePlayground";
+import  TemplateFileTree  from "@/features/playground/components/template-file-tree";
 
 
 const Page = () => {
     const {id} = useParams<{id:string}>();
     const {playgroundData, templateData, isLoading, error, loadPlayground, saveTemplateData} = usePlayground(id);
+      const {
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    closeFile,
+    editorContent,
+    updateFileContent,
+    handleAddFile,
+    handleAddFolder,
+    handleDeleteFile,
+    handleDeleteFolder,
+    handleRenameFile,
+    handleRenameFolder,
+    openFiles,
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+  } = useFileExplorer();
 
-    console.log(templateData);
 
     return (
         <div>
             <>
+            {!isLoading && templateData && (
+                <TemplateFileTree data={templateData} />
+            )}
+
             {/* TODO: TEMPLATE TREE */}
             <SidebarInset>
                 <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
